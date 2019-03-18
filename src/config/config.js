a6c6a83e8685309a4b0cd935afe20095fb6932ca
Roberto Duarte <robert0d3@outlook.com>
2019-03-18 13:12:12 -0600

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 process.env.PORT = process.env.PORT || 3000;
 
 if (process.env.NODE_ENV === 'dev') {
-	process.env.URL_DB = 'mongodb://127.0.0.1/Dashboard';
+	process.env.URL_DB = 'mongodb://127.0.0.1/dashboard';
 } else {
 	process.env.URL_DB =
 		'mongodb+srv://hanzuk:5Cq5uMscJbl3XHvX@proyectosistemasinfo3-vqjmx.mongodb.net/AnabolicGym?retryWrites=true';
