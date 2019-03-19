74c6e27b64ab10b0eaed1bc1773bfe80e990c9c1
Roberto Duarte <robert0d3@outlook.com>
2019-03-19 15:03:39 -0600

    Agregación y ruta para los gastos

---
+++ src/aggregations/_gastos.js
@@ -0,0 +1,96 @@
+const moment = require('moment');
+
+exports.annual_expenses = year => {
+	return [
+		{
+			$match: {
+				fecha: {
+					$gte: new Date(
+						moment
+							.utc()
+							.set('year', year)
+							.startOf('year')
+					),
+					$lte: new Date(
+						moment
+							.utc()
+							.set('year', year)
+							.endOf('year')
+					)
+				}
+			}
+		},
+		{
+			$group: {
+				_id: {
+					year: { $year: '$fecha' },
+					month: { $month: '$fecha' }
+				},
+				monto: { $sum: '$monto' }
+			}
+		},
+		{
+			$project: {
+				_id: 0,
+				monto: 1,
+				fecha: {
+					$dateFromParts: {
+						year: '$_id.year',
+						month: '$_id.month',
+						day: 10
+					}
+				}
+			}
+		},
+		{
+			$sort: { fecha: 1 }
+		}
+	];
+};
+
+exports.monthly_expenses = (year, month) => {
+	return [
+		{
+			$match: {
+				fecha: {
+					$gte: new Date(
+						moment
+							.utc()
+							.set('year', year)
+							.set('month', month - 1)
+							.startOf('month')
+					),
+					$lte: new Date(
+						moment
+							.utc()
+							.set('year', year)
+							.set('month', month - 1)
+							.endOf('month')
+					)
+				}
+			}
+		},
+		{
+			$group: {
+				_id: {
+					year: { $year: '$fecha' },
+					month: { $month: '$fecha' }
+				},
+				total: { $sum: '$monto' }
+			}
+		},
+		{
+			$project: {
+				_id: 0,
+				total: 1,
+				fecha: {
+					$dateFromParts: {
+						year: '$_id.year',
+						month: '$_id.month',
+						day: 10
+					}
+				}
+			}
+		}
+	];
+};
--- src/routes/gastos.js
+++ src/routes/gastos.js
@@ -4,6 +4,47 @@ const router = express.Router();
 const Gasto = require('../models/Gasto');
 const GastoAggr = require('../aggregations/_gastos');
 
+const apiResponse = (req, res, err, data) => {
+	if (err) {
+		res.status(500).send({
+			error: `☠️ Internal Server Error ${err.message}`
+		});
+	} else {
+		if (data) {
+			if (data.length > 0) {
+				res.status(200).send(data);
+			} else {
+				res.status(404).send({
+					error: '😭 Not found'
+				});
+			}
+		} else {
+			res.status(404).send({
+				error: '☠️ Not found'
+			});
+		}
+	}
+};
+
+router.get('/:year/:month', async (req, res) => {
+	if (req.params.month === 'all')
+		return await Gasto.aggregate(
+			GastoAggr.annual_expenses(parseInt(req.params.year))
+		).exec((err, data) => apiResponse(req, res, err, data));
+
+	if (parseInt(req.params.month) < 1 || parseInt(req.params.month) > 12)
+		return res.status(400).send({
+			error: 'Bad Request'
+		});
+
+	await Gasto.aggregate(
+		GastoAggr.monthly_expenses(
+			parseInt(req.params.year),
+			parseInt(req.params.month)
+		)
+	).exec((err, data) => apiResponse(req, res, err, data));
+});
+
 router.post('/load', async (req, res) => {
 	const randomDate = () => {
 		let startDate = new Date('2014-01-01').getTime();
