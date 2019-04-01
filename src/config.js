9db79c29c039fe9a962ade32c4bad2e90c88d798
Roberto Duarte <robert0d3@outlook.com>
2019-04-01 00:38:17 -0600

    Config

---
+++ src/config.js
@@ -0,0 +1,9 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+process.env.PORT = process.env.PORT || 3000
+
+if (process.env.NODE_ENV === 'dev') {
+	process.env.URL_DB = 'mongodb://127.0.0.1/dashboard_dw'
+} else {
+	process.env.URL_DB =
+		'mongodb+srv://hanzuk:5Cq5uMscJbl3XHvX@proyectosistemasinfo3-vqjmx.mongodb.net/AnabolicGym?retryWrites=true'
+}
