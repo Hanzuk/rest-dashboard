6761db5d92532bfb0083fdab794a29e536ff4136
Roberto Duarte <robert0d3@outlook.com>
2019-04-01 00:46:02 -0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -5,5 +5,5 @@ if (process.env.NODE_ENV === 'dev') {
 	process.env.URL_DB = 'mongodb://127.0.0.1/dashboard_dw'
 } else {
 	process.env.URL_DB =
-		'mongodb+srv://hanzuk:5Cq5uMscJbl3XHvX@proyectosistemasinfo3-vqjmx.mongodb.net/AnabolicGym?retryWrites=true'
+		'mongodb+srv://hanzuk:5Cq5uMscJbl3XHvX@proyectosistemasinfo3-vqjmx.mongodb.net/test?retryWrites=true'
 }
